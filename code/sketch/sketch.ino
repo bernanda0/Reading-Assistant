a7e5fc0b8b237fdaeb4ddb540b2ff119b7ea351e
bernanda <bernanda.nautval@ui.ac.id>
2023-05-15 18:16:03 +0700

    Fixing SPI 7Seg Code

---
+++ code/sketch/sketch.ino
@@ -0,0 +1,14 @@
+// extern "C"
+// {
+//   void start();
+//   void again();
+//   void led_off();
+// }
+
+// void setup() {
+//   start();
+//   attachInterrupt(digitalPinToInterrupt(2), led_off, FALLING);
+// }
+
+// void loop() {
+// }
